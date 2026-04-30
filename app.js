422ff2114a3fb7d88ef5e7c4b44a5b1ef6cf4d4e
belelalu <bele.lalu@gmail.com>
2026-04-30 15:50:34 +0700

    Perubahan semua file

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 document.getElementById("btnLoad").addEventListener("click", function () {
-    alert("JS dari file terpisah jalan");
+
+    const url = "https://script.google.com/macros/s/AKfycbzooPJLIUemyCzw1ff-x47N1J8rhXajd458I3KWfNRq8nY67ulNIQqRAIuKZA2ECsEl/exec";
+  
+    const params = new URLSearchParams({
+      api: "1",
+      action: "read",
+      payload: JSON.stringify({
+        sheetName: "Users"
+      })
+    });
+  
+    fetch(url + "?" + params.toString())
+      .then(res => res.json())
+      .then(data => {
+        document.getElementById("output").innerText =
+          JSON.stringify(data, null, 2);
+      })
+      .catch(err => {
+        document.getElementById("output").innerText = "Error: " + err;
+      });
+  
   });
